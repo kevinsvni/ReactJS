5fd57db23a23e36c04af3318c65f09e791f05b6b
kevinsvni <kvnsavani@gmail.com>
2023-09-14 15:02:03 -0400

    Tutorial 26

--- src/components/purecomponents/ParentComp.js
+++ src/components/purecomponents/ParentComp.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import RegComp from './RegComp';
 import PureComp from './PureComp';
+import MemoComp from './MemoComp';
 
 class ParentComp extends Component {
     constructor(props) {
@@ -27,6 +28,7 @@ class ParentComp extends Component {
                 <h4>Parent Component</h4>
                 <RegComp name={this.state.name}/>
                 <PureComp name={this.state.name}/>
+                <MemoComp name={this.state.name}/>
             </div>
         )
     }
