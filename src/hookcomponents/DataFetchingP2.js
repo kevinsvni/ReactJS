ea2a56b83aed1d2284f9dedfad4c3d1e81f8edb8
kevinsvni <kvnsavani@gmail.com>
2023-10-02 13:54:49 -0400

    HookTutorial 14 - Fetching data with useEffect Part 3

--- src/hookcomponents/DataFetchingP2.js
+++ src/hookcomponents/DataFetchingP2.js
@@ -4,33 +4,29 @@ import axios from 'axios'
 function DataFetchingP2() {
     const [post, setPost] = useState({})
     const [id, setId] = useState(1)
+    const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+    const handleClick = () => {
+        setIdFromButtonClick(id)
+    }
     useEffect(() => {
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+        axios.get(`https://jsonplaceholder.typicode.com/posts/${idFromButtonClick}`)
             .then(response => {
                 console.log(response)
                 setPost(response.data)
             })
             .catch(error => {
                 console.log(error)
-                // this.setState({ errorMsg: 'Error retreiving data' })
             })
-    }, [id])
+    }, [idFromButtonClick])
     return (
         <div>
             <input type='text' value={id} onChange={e => setId(e.target.value)} />
+            <button onClick={handleClick}>Fetch Post</button>
             <table>
                 <tr>
                     <th>Id</th>
                     <th>Title</th>
                 </tr>
-                {/* {
-                    posts.length ?
-                        posts.map(post => <tr>
-                            <td>{post.id}</td>
-                            <td>{post.title}</td>
-                        </tr>) :
-                        null
-                } */}
                 <tr>
                     <td>{post.id}</td>
                     <td>{post.title}</td>
